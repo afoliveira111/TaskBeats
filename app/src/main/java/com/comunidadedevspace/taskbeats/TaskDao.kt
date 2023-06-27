4dac8edd52cdbc34beb1e334d92d53bc4056bd7e
Felipe <felipetermometria@hotmail.com>
2023-06-26 22:32:26 -0300

    Initial Commit

---
+++ app/src/main/java/com/comunidadedevspace/taskbeats/TaskDao.kt
@@ -0,0 +1,32 @@
+package com.comunidadedevspace.taskbeats
+
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import androidx.room.Update
+
+
+@Dao
+interface TaskDao {
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun insert(task: Task)
+
+    @Query("Select * from task")
+    fun getAll(): List<Task>
+
+
+    @Update(onConflict = OnConflictStrategy.REPLACE)
+    fun update(task: Task)
+
+    //Deletando todos
+    @Query("DELETE from task")
+    fun deleteAll()
+
+    //Deletando pelo ID
+    @Query("DELETE from task WHERE id =:id")
+    fun deleteById(id: Int)
+
+}
